dfb2252132b185764e00dede9ecade21dde3df10
Luis Cardozo <me@luiscardozo.dev>
2020-10-26 19:21:48 -0300

    Resaltar nombre de los alumnos

--- src/hidecolumns.js
+++ src/hidecolumns.js
@@ -1,36 +1,44 @@
 console.log('inyectando...');
 
 function width_column(colNr, width){
-	tbl = document.getElementsByTagName('table')[2];
-	tr = tbl.getElementsByTagName("tr");
-	for (i = 0; i < tr.length; i++) {
+	var tbl = document.getElementsByTagName('table')[2];
+	var tr = tbl.getElementsByTagName("tr");
+	for (var i = 0; i < tr.length; i++) {
 
-	    if(i == 0) elem = 'th'; else elem = 'td';
+		var elem = 'td';
+	    if(i == 0) elem = 'th';
 
-	    tds = tr[i].getElementsByTagName(elem);
+	    var tds = tr[i].getElementsByTagName(elem);
 	    if(tds !== 'undefined' && tds.length > 0) {
-		td = tds[colNr];
-		if(td){
-			td.style.width = width;
-		}
+			var td = tds[colNr];
+			if(td){
+				td.style.width = width;
+			}
 	    }
 	}
 }
 
 function un_hide_column(colNr, display){
-	tbl = document.getElementsByTagName('table')[2];
-	tr = tbl.getElementsByTagName("tr");
-	for (i = 0; i < tr.length; i++) {
+	for(var idNr = 1; idNr <= 20; idNr++){
+		var div = document.getElementById(idNr.toString());
+		if(!div) continue;
 
-	    if(i == 0) elem = 'th'; else elem = 'td';
+		var tbl = div.getElementsByTagName('table')[0];
+		var tr = tbl.getElementsByTagName("tr");
+		for (var i = 0; i < tr.length; i++) {
 
-	    tds = tr[i].getElementsByTagName(elem);
-	    if(tds !== 'undefined' && tds.length > 0) {
-		td = tds[colNr];
-		if(td){
-			td.style.display = display;
+			var elem = 'td';
+			if(i == 0) elem = 'th';
+
+			var tds = tr[i].getElementsByTagName(elem);
+			if(tds !== 'undefined' && tds.length > 0) {
+				var td = tds[colNr];
+				if(td){
+					td.style.display = display;
+					console.log(`setting column ${colNr} to ${display}`);
+				}
+			}
 		}
-	    }
 	}
 }
 
@@ -42,7 +50,33 @@ function hide_column(colNr){
 	un_hide_column(colNr, 'none');
 }
 
-hide_column(1); //Matricula
+function resaltar_nombre(){
+	console.log("Resaltar nombres");
+	for(var idNr = 1; idNr <= 20; idNr++){
+		var div = document.getElementById(idNr.toString());
+		if(!div) continue;
+
+		var tbl = div.getElementsByTagName('table')[0];
+		var tr = tbl.getElementsByTagName("tr");
+		//recorremos cada fila
+		for (var i = 0; i < tr.length; i++) {
+			if(i == 0) continue;	//saltamos th
+			
+			var tds = tr[i].getElementsByTagName('td');
+			if(tds !== 'undefined' && tds.length > 0) {
+				//estamos dentro de un row válido.
+				var td_Nombre = tds[3];		//la columna 3 es la de nombres
+				if(td_Nombre){
+					const nameSpl = td_Nombre.innerHTML.split(',');
+					td_Nombre.innerHTML = nameSpl[0] + ', <b style="color:green;">' + nameSpl[1] + '</b>';
+				}
+			}
+		}
+	}
+}
+
+resaltar_nombre();
+hide_column(2); //Matricula
 //hide_column(4); //Titulo
 hide_column(5); //Tipo
 hide_column(6); //Archivo
