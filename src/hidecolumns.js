8c238639c7062a83dbdc71e5b1420c81aad3c463
Luis Cardozo <luiscardozocarreras@gmail.com>
2021-09-11 13:45:35 -0400

    Avoiding focus on unneeded inputs

--- src/hidecolumns.js
+++ src/hidecolumns.js
@@ -42,6 +42,41 @@ function un_hide_column(colNr, display){
 	}
 }
 
+function unfocusable_column(colNr, display){
+	for(var idNr = 1; idNr <= 20; idNr++){
+		var div = document.getElementById(idNr.toString());
+		if(!div) continue;
+
+		var tbl = div.getElementsByTagName('table')[0];
+		var tr = tbl.getElementsByTagName("tr");
+		//recorremos cada fila
+		for (var i = 0; i < tr.length; i++) {
+			if(i == 0) continue;	//saltamos th
+			
+			var tds = tr[i].getElementsByTagName('td');
+			if(tds !== 'undefined' && tds.length > 0) {
+				//estamos dentro de una row válido.
+				var td_Nombre = tds[colNr];
+				if(td_Nombre){
+					const lnk = td_Nombre.getElementsByTagName('a');
+					if(lnk !== 'undefined') {
+						if(lnk[0]) {
+							lnk[0].tabIndex = -1;
+						}
+					}
+					const inpt = td_Nombre.getElementsByTagName('input');
+					if(inpt !== 'undefined') {
+						if(inpt[1]) {
+							inpt[1].tabIndex = -1;
+						}
+					}
+					console.log(`unfocussing column ${colNr} to ${display}`);
+				}
+			}
+		}
+	}
+}
+
 function unhide_column(colNr){
 	un_hide_column(colNr, '');
 }
@@ -52,7 +87,7 @@ function hide_column(colNr){
 
 function resaltar_nombre(){
 	console.log("Resaltar nombres");
-	for(var idNr = 1; idNr <= 20; idNr++){
+	for(var idNr = 1; idNr <= 20; idNr++){		//cada semana tiene un div id="${idNr}"
 		var div = document.getElementById(idNr.toString());
 		if(!div) continue;
 
@@ -77,7 +112,9 @@ function resaltar_nombre(){
 
 resaltar_nombre();
 hide_column(2); //Matricula
-//hide_column(4); //Titulo
+//hide_column(4); //Titulo //--desenfocar: 4, 7,
+unfocusable_column(4, 'none');
+unfocusable_column(7, 'none');
 hide_column(5); //Tipo
 hide_column(6); //Archivo
 
